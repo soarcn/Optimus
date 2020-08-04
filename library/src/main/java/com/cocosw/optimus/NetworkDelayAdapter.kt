82e0493f1ea45ac67809b5ae02cccff38a68fbd2
Kai <soarcn@gmail.com>
2020-08-04 16:46:32 +1000

    fix delay time unit issue

--- library/src/main/java/com/cocosw/optimus/NetworkDelayAdapter.kt
+++ library/src/main/java/com/cocosw/optimus/NetworkDelayAdapter.kt
@@ -8,7 +8,7 @@ import android.widget.TextView
 
 internal class NetworkDelayAdapter(context: Context) : BindableAdapter<Long>(context) {
 
-    private val VALUES = longArrayOf(250, 500, 1000, 2000, 3000, 5000)
+    private val VALUES = longArrayOf(0, 250, 500, 1000, 2000, 3000, 5000)
 
     fun getPosition(value: Long): Int {
         for (i in VALUES.indices) {
