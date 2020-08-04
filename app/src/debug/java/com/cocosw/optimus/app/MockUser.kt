eb9827ffe717c1f4ec993fe99abf544121cf581f
Kai <soarcn@gmail.com>
2020-08-04 16:48:31 +1000

    update demo application layout

--- app/src/debug/java/com/cocosw/optimus/app/MockUser.kt
+++ app/src/debug/java/com/cocosw/optimus/app/MockUser.kt
@@ -13,8 +13,9 @@ val mockApi = alter(Api::class.java, "Api") {
 class MockUser : MockResponse {
     @Default
     val HTTP200 = success {}
-    val HTTP401 = error(code = 401)
-    val NOTFOUND = error { ApiError(404, "Can not find your account") }
+    val HTTP401 = error(401) { ApiError(401, "Unauthorized") }
+    val NOTFOUND = error(404) { ApiError(404, "Can not find your account") }
     val LOCKED = error { ApiError(400, "Your account been locked") }
-    val EXPIRED = error { ApiError(400, "Your account is expired, please contact us via email xxx@xx.com") }
+    val EXPIRED =
+        error { ApiError(400, "Your account is expired, please contact us via email xxx@xx.com") }
 }
